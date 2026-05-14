90f6f31033097d627f4db1101898ee3ec80c0c8e
abrahambishopcodes <crypticcodetechnologies@gmail.com>
2026-05-15 00:47:39 +0100

    feat: add health check route, standardize API responses, and update recommendation query parameters

--- task-2-smart-recommendation-agent/src/routes/index.route.ts
+++ task-2-smart-recommendation-agent/src/routes/index.route.ts
@@ -1,18 +1,31 @@
 import { Router, Request, Response } from "express";
 import { index } from "../config/pinecone.config";
+import { sendSuccessResponse } from "../utils/apiResponseHelpers";
 
 const router = Router();
+
+// health check
+router.get("/health", (req: Request, res: Response) => {
+    sendSuccessResponse(res, 200, "OK", null);
+});
 
 router.post("/recommend", async (req: Request, res: Response) => {
     const {persona_text} = req.body;
 
     const pcQueryResults = await index.searchRecords({
         query: {
-            inputs: persona_text,
-            topK: 20,
+            inputs: {
+                text: persona_text
+            },
+            topK: 5,
         },
         
     })
+
+    sendSuccessResponse(res, 200, "Recommendations fetched successfully", {
+        queryResults: pcQueryResults,
+
+    });
 
 })
 
--- task-2-smart-recommendation-agent/src/server.ts
+++ task-2-smart-recommendation-agent/src/server.ts
@@ -10,14 +10,8 @@ const PORT = getEnv("PORT");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// routes
 app.use("/api", router);
-
-// health check
-app.get("/health", (req, res) => {
-    res.json({
-        message: "OK",
-    });
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
